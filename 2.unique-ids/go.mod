78f93417be60ec50a5b1aa674e664566ec428bf8
Tamim Ehsan <tamimehsan99@gmail.com>
2024-09-20 20:13:37 +0600

    complete

---
+++ 2.unique-ids/go.mod
@@ -0,0 +1,8 @@
+module maelstrom-unique-ids
+
+go 1.23.1
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jepsen-io/maelstrom/demo/go v0.0.0-20240813160128-8b9e94c75e59 // indirect
+)
---
+++ 2.unique-ids/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/google/uuid"
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+type server struct {
+	node *maelstrom.Node
+}
+
+func (s *server) handleUniqueIds(msg maelstrom.Message) error {
+	var body map[string]any
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return err
+	}
+
+	// Update the message type to return back.
+	body["type"] = "generate_ok"
+	body["id"] = uuid.New().String()
+
+	// Echo the original message back with the updated message type.
+	return s.node.Reply(msg, body)
+}
+
+func main() {
+	s := &server{}
+	s.node = maelstrom.NewNode()
+
+	s.node.Handle("generate", s.handleUniqueIds)
+	if err := s.node.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
